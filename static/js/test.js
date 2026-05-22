24c0060eaf08355b06678e8e4a1142c549c21970
absol255 <mewoisthebest123456789@gmail.com>
2026-05-21 22:36:53 -0700

    aisdj

--- static/js/test.js
+++ static/js/test.js
@@ -1,4 +1,5 @@
 const cards = document.querySelectorAll(".card");
+const form = document.querySelector("form");
 
 cards.forEach(card => {
     const radios = card.querySelectorAll(".exclusive");
@@ -12,4 +13,36 @@ cards.forEach(card => {
             });
         });
     });
+});
+
+form.addEventListener("submit", (e) => {
+
+    const cards = document.querySelectorAll(".card");
+
+    let allAnswered = true;
+
+    cards.forEach(card => {
+        const radios = card.querySelectorAll('input[type="radio"]');
+
+        let answered = false;
+
+        radios.forEach(radio => {
+            if (radio.checked) {
+                answered = true;
+            }
+        });
+
+        if (!answered) {
+            allAnswered = false;
+            card.style.border = "2px solid red"; // optional visual warning
+        } else {
+            card.style.border = "";
+        }
+    });
+
+    if (!allAnswered) {
+        e.preventDefault();
+        alert("You must answer every question!");
+    }
+
 });
